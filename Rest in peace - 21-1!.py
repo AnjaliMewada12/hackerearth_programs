4b9a555fb2e241e9c58e634adb18eb2ce9c64d95
Anjali Mewada <66212029+AnjaliMewada12@users.noreply.github.com>
2020-06-26 14:40:53 +0530

    Create Rest in peace - 21-1!.py

---
+++ Rest in peace - 21-1!.py
@@ -0,0 +1,25 @@
+'''The grandest stage of all, Wrestlemania XXX recently happened. And with it, happened one of the biggest heartbreaks for the WWE fans around the world. The Undertaker's undefeated streak was finally over.
+
+Now as an Undertaker fan, you're disappointed, disheartened and shattered to pieces. And Little Jhool doesn't want to upset you in any way possible. (After all you are his only friend, true friend!) Little Jhool knows that you're still sensitive to the loss, so he decides to help you out.
+
+Every time you come across a number, Little Jhool carefully manipulates it. He doesn't want you to face numbers which have "21" as a part of them. Or, in the worst case possible, are divisible by 21.
+
+If you end up facing such a number you feel sad... and no one wants that - because you start chanting "The streak is broken!" , if the number doesn't make you feel sad, you say, "The streak lives still in our heart!"
+
+Help Little Jhool so that he can help you!
+
+Input Format:
+The first line contains a number, t, denoting the number of test cases.
+After that, for t lines there is one number in every line.
+
+Output Format:
+Print the required string, depending on how the number will make you feel.'''
+
+test=int(input())
+for i in range(0,test):
+    n=input()
+    if n.find('21')==-1 and int(n)%21!=0:
+        print("The streak lives still in our heart!")
+    else:
+        print("The streak is broken!")
+    
