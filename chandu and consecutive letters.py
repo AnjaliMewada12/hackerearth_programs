e25a588160d92cf973e20853e400660c736e650e
Anjali Mewada <66212029+AnjaliMewada12@users.noreply.github.com>
2020-06-26 08:57:46 +0530

    Update chandu and consecutive letters.py

--- chandu and consecutive letters.py
+++ chandu and consecutive letters.py
@@ -1,4 +1,4 @@
-que.  Chandu is very fond of strings. (Or so he thinks!) But, he does not like strings which have same consecutive letters. No one has any idea why it is so. He calls these strings as Bad strings. So, Good strings are the strings which do not have same consecutive letters. Now, the problem is quite simple. Given a string S, you need to convert it into a Good String.
+'''que.  Chandu is very fond of strings. (Or so he thinks!) But, he does not like strings which have same consecutive letters. No one has any idea why it is so. He calls these strings as Bad strings. So, Good strings are the strings which do not have same consecutive letters. Now, the problem is quite simple. Given a string S, you need to convert it into a Good String.
 
 You simply need to perform one operation - if there are two same consecutive letters, delete one of them.
 
@@ -9,7 +9,7 @@ Each test case consists of a string S, which consists of only lower case letters
 Output:
 For each test case, print the answer to the given problem.
 
-Ans  --->
+Ans  --->'''
 
 t=int(input())
 for i in range(t):
