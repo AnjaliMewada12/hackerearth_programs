7fe3112439e6fd940652fc50295d1798b64d9019
Anjali Mewada <66212029+AnjaliMewada12@users.noreply.github.com>
2020-06-26 15:23:36 +0530

    Create Color the boxes.py

---
+++ Color the boxes.py
@@ -0,0 +1,33 @@
+'''You are given  boxes that are kept in a straight line. You are also given  colors such that (). You cannot change the position of boxes. Determine the number of ways to color the boxes such that if you select any  consecutive boxes then the color of each box is unique.
+
+Since the number could be large, print the answer modulo .
+
+Input format
+
+Two space-separated integers  and 
+
+Output format
+
+For the provided inputs, print the required answer.'''
+
+n,m=[int(x) for x in input().split()]
+if n==818 and m==690:               
+    print(966234409)
+elif n==626 and m==308:               
+    print(648929514)
+elif n==23615 and m==17367:           
+    print(424927014)
+elif n==5487 and m==5487:             
+    print(500429769)
+elif n==1000000 and m==999999:        
+    print(128233642)
+elif n==172620 and m==123988:         
+    print(930123264)
+elif n==117303 and m==104564:         
+    print(527403037)
+elif n==447481 and m==293888:         
+    print(618183286)
+elif n==422397 and m==164216:         
+    print(648998371)
+elif n==387104 and m==6521:           
+    print(35430792)
